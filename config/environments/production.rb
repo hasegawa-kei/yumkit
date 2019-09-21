1c774397e3d786f00a1dc33ca5cb8c3b3dce1238
hasegawa kei <yahooku.kei@gmail.com>
2019-09-22 01:44:07 +0900

    syusei

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -78,9 +78,9 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.initialize_on_precompile = false
 
-  # $ bin/rails assets:precompile を有効にするため
-  config.assets.compile = true  # falseからtrueに変更
+
 
   # JavaScriptのES6に対応させるため
   config.assets.js_compressor = Uglifier.new(harmony: true)
